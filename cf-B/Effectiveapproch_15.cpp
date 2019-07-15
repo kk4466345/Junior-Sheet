5fa73bf7df2446023ce3c30d5e912f9f6d283370
kk4466345 <kk4466345@gmail.com>
2019-07-15 06:34:01 +0530

    effective approch is not working ----time limit exceed

---
+++ cf-B/Effectiveapproch_15.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+using std::cout;
+int find(int num,int* a,int n) {
+	for(int i=0;i<n;i++) {
+		if(a[i] == num) {
+			return i+1;
+		}
+	}
+	return 0;
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+	int num[n];
+	for(int i=0;i<n;i++) {
+		cin >> num[i];
+	}
+	int m;
+	cin >> m;
+	int a[m];
+	for(int i=0;i<m;i++) {
+		cin >> a[i];
+	}
+
+	int start=0;
+	int end=0;
+	for(int i=0;i<m;i++) {
+		int s=find(a[i],num,n);
+		end=end+n-start+1;
+		start=start+s;
+	}
+	cout << start;
+	printf(" ");
+	cout << end;
+
+	return 0;
+}
